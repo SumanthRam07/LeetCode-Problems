b4ac737b95edb29ba69707b588ed1d35aa51b5e8
SumanthRam07 <sumanthgollaprolu07@gmail.com>
2024-06-20 00:05:38 -0400

    19-jun-2024/3

---
+++ .history/June-19-2024/code_20240620000511.java
@@ -0,0 +1,53 @@
+package June-19-2024;
+
+public class code {
+
+
+    class Solution {
+        public ListNode deleteMiddle(ListNode head) {
+            
+            
+            ListNode temp = head ; 
+            int count = 0 ; 
+            
+            
+            while( temp != null)
+            {
+                temp = temp.next ; 
+                count ++ ; 
+            }
+            
+            
+            if( count == 1)
+            {
+                return null  ; 
+            }
+            
+            else
+            {   
+                temp = head ; 
+                
+                count = count /2 ; 
+                
+                int i = 0 ; 
+                
+                while( i < count-1  )
+                {
+                    
+                    temp = temp.next ; 
+                    i++ ;
+                }
+                
+                temp.next = temp.next.next ; 
+           
+                
+            }
+            
+            
+            
+            return head ; 
+            
+        }
+    }
+    
+}
