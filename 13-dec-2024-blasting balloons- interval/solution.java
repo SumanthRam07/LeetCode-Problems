dc54e72a7374276b4a414aa2136f71f4c3ef2050
SumanthRam07 <sumanthgollaprolu07@gmail.com>
2024-12-13 18:55:04 -0500

    13-dec-2024

---
+++ 13-dec-2024-blasting balloons- interval/solution.java
@@ -0,0 +1,32 @@
+package 13-dec-2024-blasting balloons- interval;
+
+import java.util.Arrays;
+
+class Solution {
+    public int findMinArrowShots(int[][] points) {
+       
+      
+        Arrays.sort(points , (a, b) -> Integer.compare(a[1]  , b[1])) ; 
+        
+        int end = points[0][1] ; 
+        
+        int arrows = 1 ; 
+        
+        for( int i = 1 ; i < points.length ; i++)
+        {
+            
+            int tempStart = points[i][0] ; 
+            
+            if(tempStart > end  ) 
+            {
+                arrows++ ; 
+                
+                end = points[i][1] ;    
+            }
+                      
+        }
+            
+        return arrows ; 
+        
+    }
+}
